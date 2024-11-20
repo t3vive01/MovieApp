e241c49cc0afdae547410fd3a23de8e0819238eb
iconshah <shahicon691@gmail.com>
2024-11-20 11:39:33 +0200

    minor chsnges

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from '../src/screens/App.js';
-import reportWebVitals from './reportWebVitals';
+import reportWebVitals from './reportWebVitals.js';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
