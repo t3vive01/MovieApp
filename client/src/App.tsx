310335941eebd164dea99752dacea284f02337c7
t3vive01 <t3vive01@students.oamk.fi>
2024-11-28 22:26:02 +0200

    Front changes for what we did with Kasidhad

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,14 +1,13 @@
 import { Route, Routes } from "react-router-dom";
 import "./App.css";
 import { Navbar } from "./components/navbar/navbar";
-import { About, Contact, Home } from "./components/pages";
+import { About, Contact } from "./components/pages";
 
 function App() {
   return (
     <div className="App">
       <Navbar />
       <Routes>
-        <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
         <Route path="/contact" element={<Contact />} />
       </Routes> 
