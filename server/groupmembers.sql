3c2308a3369ca825a8f7bee7afa4b0cd02a45377
Kasidhad <kkasidhad@gmail.com>
2024-11-28 22:09:20 +0700

    deleting unecesaary files

--- server/groupmembers.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE GroupMembers (
-    group_member_id SERIAL PRIMARY KEY,
-    group_id INT REFERENCES Groups(group_id) ON DELETE CASCADE,
-    user_id INT REFERENCES Users(user_id) ON DELETE CASCADE,
-    joined_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-);
