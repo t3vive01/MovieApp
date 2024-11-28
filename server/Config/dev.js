7fd7511a4d36c8f4c92dba007aa626274e927aad
Kasidhad <kkasidhad@gmail.com>
2024-11-28 22:19:51 +0700

    fixing authorization

---
+++ server/Config/dev.js
@@ -0,0 +1,7 @@
+module.exports = {
+    mongoURI: 'mongodb+srv://Group19Project:MovieAppG19@group19server.ndwh3.mongodb.net/Group19Server',
+};
+
+//REMINDER INCASE!!
+//password: MovieAppG19
+// if theres problems with db just message me
